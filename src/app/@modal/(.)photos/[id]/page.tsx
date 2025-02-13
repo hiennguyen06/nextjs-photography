65d5e00f86ec2675b016ae519af7a089ecb9a903
Hien Nguyen <hiennguyen1529@gmail.com>
2025-02-13 19:47:21 +1100

    add type for image in modal page

--- src/app/@modal/(.)photos/[id]/page.tsx
+++ src/app/@modal/(.)photos/[id]/page.tsx
@@ -1,12 +1,13 @@
 import { Modal } from "@/app/components/Modal";
 import getPhotos from "@/app/lib/getPhotos";
 import { buildImageUrl } from "@/app/lib/utils";
+import { type ImageProps } from "@/app/lib/types";
 import Image from "next/image";
 
 const PhotoModal = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
   const images = await getPhotos();
-  const image = images.find((img) => img.id === id);
+  const image = images.find((img: ImageProps) => img.id === id);
 
   const imageUrl = buildImageUrl(1280, image.public_id, image.format);
   console.log(image);
